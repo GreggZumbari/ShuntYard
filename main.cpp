82ab5092b30eda6a46c6089001d84d4e52d68721
Gregg H <50878128+GreggZumbari@users.noreply.github.com>
2020-01-30 10:49:35 -0800

    10:49 AM 1/30/2020

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/*
+
+ */
 #include <iostream>
 #include <cstring>
 
@@ -5,12 +8,20 @@ using namespace std;
 
 int main() {
 
-  char* in = new char[1000];
+	char* in = new char[1000];
   
-  cout << "Hello, user. Please enter in your equation, using infix format, separated by spaces (e.x. 3 + 4): ";
+	cout << "Hello, user. Please enter in your equation, using infix format, separated by spaces (e.x. 3 + 4): ";
 
-  cin.getline(in, 1000);
+	cin.getline(in, 1000);
 
-  cout << strlen(in) << endl;
+	//Only allow numbers and operators
+	for (int i = 0; i < strlen(in); i++) {
+		if (in[i] <= 39 ||
+		in[i] == 44) {
+    
+	}
+}
+  
+	cout << in << endl;
   
 }
